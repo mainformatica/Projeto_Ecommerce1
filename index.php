b2311db2f195ecadb489172617799722e8def86c
mainformatica <msantos@mainfo.com.br>
2018-03-22 11:58:43 -0300

    aulas

---
+++ index.php
@@ -0,0 +1,167 @@
+<?php 
+session_start();
+
+require_once("vendor/autoload.php");
+
+use \Slim\Slim;
+use \Hcode\Page;
+use \Hcode\PageAdmin;
+use \Hcode\Model\User;
+
+
+$app = new Slim();
+
+$app->config('debug', true);
+
+$app->get('/', function() {
+    
+    $page = new Page();
+
+    $page->setTpl("index");
+});
+
+$app->get('/admin', function() {
+    
+User::verifyLogin();
+
+    $page = new PageAdmin();
+
+    $page->setTpl("index");
+
+});
+
+$app->get('/admin/login', function() {
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+]);
+
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function(){
+
+User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+	exit;
+	
+});
+//finaliza sessão / sai da pagina
+$app->get('/admin/logout', function(){
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
+});
+//rota para abrir pagina usuarios
+$app->get("/admin/users", function(){
+
+    User::verifyLogin();
+
+    $users = User::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users", array(
+        "users"=>$users
+	));
+
+});
+//rota para criar usuario
+$app->get("/admin/users/create", function(){
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-create");
+
+});
+//apaga usuario
+$app->get("/admin/users/:iduser/delete", function($iduser){
+
+    User::verifyLogin();
+
+    $user = new User();
+
+    $user->get((int)$iduser);
+
+    $user->delete();
+
+    header("Location: /admin/users");
+    exit;
+
+    });
+//rota para alterar usuario
+$app->get("/admin/users/:iduser", function($iduser){
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-update", array(
+"user"=>$user->getValues()
+	));
+
+});
+//altera usuario
+$app->post("/admin/users/create", function(){
+
+    User::verifyLogin();
+
+    $user = new User();
+
+    $_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+    $user->setData($_POST);
+
+    $user->save();
+    
+    header("Location: /admin/users");
+    exit;
+
+});
+//cria usuario
+$app->post("/admin/users/:iduser", function($iduser){
+
+    User::verifyLogin();
+
+    $user = new User();
+
+    $_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+    $user->get((int)$iduser);
+
+    $user->setData($_POST);
+
+    $user->update();
+
+    header("Location: /admin/users");
+    exit;
+
+    });
+
+$app->get("/admin/forgot", function(){
+    
+    $page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+]);
+
+	$page->setTpl("forgot");
+
+});
+
+
+
+$app->run();
+
+ ?>
---
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace Hcode\Model;
+
+use \Hcode\DB\Sql;
+use \Hcode\Model;
+
+class user extends Model{
+
+	const SESSION = "User";
+	
+	public static function login($login, $password)                          //Tela de login
+	{
+
+		$sql = new Sql();
+         
+        $results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :LOGIN", array(
+        ":LOGIN"=>$login
+        ));
+
+        if (count($results) === 0)
+        {
+        	throw new \Exception("Usuário ou senha inválido.");
+        } 
+        
+        $data = $results[0];
+
+       if (password_verify($password, $data["despassword"]) === true)
+       {
+
+         $user = new User();
+
+         $user->setData($data);
+
+         $_SESSION[User::SESSION] = $user->getValues();
+        return $user;
+       
+
+       }else {
+         throw new \Exception("Usuário ou senha invalido.");
+         }
+
+	}
+
+	public static function verifyLogin($inadmin = true)
+	{
+
+      if(
+           !isset($_SESSION[User::SESSION])                              //Verifica a sessão foi definada
+           ||
+           !$_SESSION[User::SESSION]
+           ||
+           !(int)$_SESSION[User::SESSION]["iduser"] > 0                 //verifica se o id de usuario é valido
+           ||
+           (bool)$_SESSION[User::SESSION]["inadmin"] !== $inadmin       //Verifica se o usuario é admin
+      ){
+
+
+        header("Location: /admin/login"); //redireciona para a tela de login
+         exit;
+      }
+
+	}
+
+	public static function logout()    //Destroi a sessão e vai para tela de login
+	{
+
+		$_SESSION[User::SESSION] = NULL;
+	}
+
+  
+  public static function listAll()
+  {
+
+    $sql = new Sql;
+
+    return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
+
+  }
+
+  public function save()
+  {
+ 
+    $sql = new Sql();
+
+    $results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array( 
+      ":desperson"=>$this->getdesperson(),
+      ":deslogin"=>$this->getdeslogin(),
+      ":despassword"=>$this->getdespassword(),
+      ":desemail"=>$this->getdesemail(),
+      ":nrphone"=>$this->getnrphone(),
+      ":inadmin"=>$this->getinadmin()
+ ));
+
+    $this->setData($results[0]);
+
+  }
+
+  public function get($iduser)
+  {
+    $sql = new Sql();
+
+$results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser", array(
+     "iduser"=>$iduser
+
+));
+
+$this->setData($results[0]);
+
+}
+
+  public function update()
+  {
+
+    $sql = new Sql();
+
+    $results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array( 
+      ":iduser"=>$this->getiduser(),
+      ":desperson"=>$this->getdesperson(),
+      ":deslogin"=>$this->getdeslogin(),
+      ":despassword"=>$this->getdespassword(),
+      ":desemail"=>$this->getdesemail(),
+      ":nrphone"=>$this->getnrphone(),
+      ":inadmin"=>$this->getinadmin()
+ ));
+
+    $this->setData($results[0]);
+
+  }
+
+  public function delete()
+  {
+
+    $sql = new Sql();
+
+    $sql->select("Call sp_users_delete(:iduser)", array (
+     "iduser"=>$this->getiduser()
+
+    ));
+  }
+
+}//ultimo
+
+
+
+?>
